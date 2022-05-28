1efb8828c0506d44d96f16b17bdc64d80008ba8d
ShuheiTakahashi1224 <83751748+ShuheiTakahashi1224@users.noreply.github.com>
2022-05-28 18:39:11 +0900

    update : header routing設定

--- src/components/molecules/MenuDrawer.tsx
+++ src/components/molecules/MenuDrawer.tsx
@@ -10,22 +10,49 @@ import { memo, VFC } from "react";
 type Props = {
   isOpen: boolean;
   onClose: () => void;
+  onClickHome: () => void;
+  onClickUserManagement: () => void;
+  onClickSetting: () => void;
 };
 
 export const MenuDrawer: VFC<Props> = memo((props) => {
-  const { isOpen, onClose } = props;
+  const {
+    isOpen,
+    onClose,
+    onClickHome,
+    onClickUserManagement,
+    onClickSetting
+  } = props;
   return (
     <Drawer placement="left" size="xs" isOpen={isOpen} onClose={onClose}>
       <DrawerOverlay>
         <DrawerContent>
           <DrawerBody p={0}>
-            <Button display="block" w="100%" bg="white" borderRadius={0}>
+            <Button
+              onClick={onClickHome}
+              display="block"
+              w="100%"
+              bg="white"
+              borderRadius={0}
+            >
               TOP
             </Button>
-            <Button display="block" w="100%" bg="white" borderRadius={0}>
+            <Button
+              onClick={onClickUserManagement}
+              display="block"
+              w="100%"
+              bg="white"
+              borderRadius={0}
+            >
               ユーザ一覧
             </Button>
-            <Button display="block" w="100%" bg="white" borderRadius={0}>
+            <Button
+              onClick={onClickSetting}
+              display="block"
+              w="100%"
+              bg="white"
+              borderRadius={0}
+            >
               設定
             </Button>
           </DrawerBody>
