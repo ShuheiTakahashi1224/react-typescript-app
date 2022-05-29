5bf66315802be65bd9674fbf9ba3563e22ab2622
ShuheiTakahashi1224 <83751748+ShuheiTakahashi1224@users.noreply.github.com>
2022-05-29 14:38:12 +0900

    update : 全ユーザ読み込み 実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,18 +1,34 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react";
 
+import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
+  const { getAllUsers, users, loading } = useAllUsers();
+  useEffect(() => {
+    getAllUsers();
+  }, []);
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl={"https://source.unsplash.com/random"}
-          userName={"chanhey"}
-          fullName={"Shuhei Takahashi"}
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl={"https://source.unsplash.com/random"}
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
